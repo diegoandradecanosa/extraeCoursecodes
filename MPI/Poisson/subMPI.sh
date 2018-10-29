2269e2ab81533bfb86de4202ac398a4c8a5da712
Diego Andrade Canosa <diego.andrade@udc.es>
2018-10-29 12:27:47 +0100

    Almost finished first complete version

--- MPI/Poisson/subMPI.sh
+++ MPI/Poisson/subMPI.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 module load gcc/6.4.0 openmpi/2.1.1 extrae/3.5.2
 source /mnt/netapp1/Optcesga_FT2/opt/cesga/easybuild-cesga/software/MPI/gcc/6.4.0/openmpi/2.1.1/extrae/3.5.2/etc/extrae.sh
-export EXTRAE_CONFIG_FILE=${HOME}/extraeCoursecodes/common/extraeMPIOMP.xml
-export LD_PRELOAD=/mnt/netapp1/Optcesga_FT2/opt/cesga/easybuild-cesga/software/MPI/gcc/6.4.0/openmpi/2.1.1/extrae/3.5.2/lib/libompitrace.so
-srun --partition=cola-corta --time=00:15:00 -N 4 -n 16 ./poisson
-mv poisson.prv ${STORE}/MPItraces/poisson.prv
-mv poisson.pcf ${STORE}/MPItraces/poisson.pcf
-mv poisson.row ${STORE}/MPItraces/poisson.row	
+export EXTRAE_CONFIG_FILE=${HOME}/extraeCoursecodes/common/extraeMPI.xml
+export LD_PRELOAD=/mnt/netapp1/Optcesga_FT2/opt/cesga/easybuild-cesga/software/MPI/gcc/6.4.0/openmpi/2.1.1/extrae/3.5.2/lib/libmpitrace.so
+srun --partition=cola-corta --time=00:15:00 -N 4 -n 16 ./poisson_v0 
+mv poisson_v0.prv ${STORE}/MPItraces/poisson_v0.prv
+mv poisson_v0.pcf ${STORE}/MPItraces/poisson_v0.pcf
+mv poisson_v0.row ${STORE}/MPItraces/poisson_v0.row	
+srun --partition=cola-corta --time=00:15:00 -N 4 -n 16 ./poisson_v1
+mv poisson_v1.prv ${STORE}/MPItraces/poisson_v1.prv
+mv poisson_v1.pcf ${STORE}/MPItraces/poisson_v1.pcf
+mv poisson_v1.row ${STORE}/MPItraces/poisson_v1.row
 
 
---
+++ MPI/Prime/subMPI.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+module load gcc/6.4.0 openmpi/2.1.1 extrae/3.5.2
+source /mnt/netapp1/Optcesga_FT2/opt/cesga/easybuild-cesga/software/MPI/gcc/6.4.0/openmpi/2.1.1/extrae/3.5.2/etc/extrae.sh
+export EXTRAE_CONFIG_FILE=${HOME}/extraeCoursecodes/common/extraeMPI.xml
+export LD_PRELOAD=/mnt/netapp1/Optcesga_FT2/opt/cesga/easybuild-cesga/software/MPI/gcc/6.4.0/openmpi/2.1.1/extrae/3.5.2/lib/libmpitrace.so
+srun --partition=cola-corta --time=00:15:00 -N 4 -n 16 ./prime_v0
+mv prime_v0.prv ${STORE}/MPItraces/prime_v0.prv
+mv prime_v0.pcf ${STORE}/MPItraces/prime_v0.pcf
+mv prime_v0.row ${STORE}/MPItraces/prime_v0.row	
+srun --partition=cola-corta --time=00:15:00 -N 4 -n 16 ./prime_v1
+mv prime_v1.prv ${STORE}/MPItraces/prime_v1.prv
+mv prime_v1.pcf ${STORE}/MPItraces/prime_v1.pcf
+mv prime_v1.row ${STORE}/MPItraces/prime_v1.row
+srun --partition=cola-corta --time=00:15:00 -N 4 -n 16 ./prime_v2
+mv prime_v2.prv ${STORE}/MPItraces/prime_v2.prv
+mv prime_v2.pcf ${STORE}/MPItraces/prime_v2.pcf
+mv prime_v2.row ${STORE}/MPItraces/prime_v2.row
+
+
+
