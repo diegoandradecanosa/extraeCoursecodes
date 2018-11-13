366eca533da9158c71d7ece4a726ba27911761ee
Diego Andrade Canosa <diego.andrade@udc.es>
2018-11-13 12:35:30 +0100

    Redirected to xml files

--- MPI/Poisson/subMPI.sh
+++ MPI/Poisson/subMPI.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 module load gcc/6.4.0 openmpi/2.1.1 extrae/3.5.2
 source /mnt/netapp1/Optcesga_FT2/opt/cesga/easybuild-cesga/software/MPI/gcc/6.4.0/openmpi/2.1.1/extrae/3.5.2/etc/extrae.sh
-export EXTRAE_CONFIG_FILE=${HOME}/extraeCoursecodes/common/extraeMPI.xml
+export EXTRAE_CONFIG_FILE=/mnt/netapp1/Optcesga_FT2/opt/cesga/easybuild-cesga/software/MPI/gcc/6.4.0/openmpi/2.1.1/extrae/3.5.2/share/example/MPI/extrae.xml
 export LD_PRELOAD=/mnt/netapp1/Optcesga_FT2/opt/cesga/easybuild-cesga/software/MPI/gcc/6.4.0/openmpi/2.1.1/extrae/3.5.2/lib/libmpitrace.so
 srun --partition=thinnodes --time=00:15:00 -N 4 -n 16 ./poisson_v0 
 mv poisson_v0.prv ${STORE}/MPItraces/poisson_v0.prv
