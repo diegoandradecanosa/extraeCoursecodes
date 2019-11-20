3d5a52e2be2186d3ac8ac44e24bc9ef8601218cf
Diego Andrade <diego.andrade@udc.es>
2019-11-20 11:46:58 +0100

    Some adaptations to local folder

--- common/subOMP.sh
+++ common/subOMP.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-module load gcc/6.4.0 openmpi/2.1.1 extrae/3.5.2
-source /mnt/netapp1/Optcesga_FT2/opt/cesga/easybuild-cesga/software/MPI/gcc/6.4.0/openmpi/2.1.1/extrae/3.5.2/etc/extrae.sh
-export EXTRAE_CONFIG_FILE=/mnt/netapp1/Optcesga_FT2/opt/cesga/easybuild-cesga/software/MPI/gcc/6.4.0/openmpi/2.1.1/extrae/3.5.2/share/example/OMP/extrae.xml
-export LD_PRELOAD=/mnt/netapp1/Optcesga_FT2/opt/cesga/easybuild-cesga/software/MPI/gcc/6.4.0/openmpi/2.1.1/extrae/3.5.2/lib/libomptrace.so
+#module load gcc/6.4.0 openmpi/2.1.1 extrae/3.5.2
+source /usr/local/etc/extrae.sh
+#source /mnt/netapp1/Optcesga_FT2/opt/cesga/easybuild-cesga/software/MPI/gcc/6.4.0/openmpi/2.1.1/extrae/3.5.2/etc/extrae.sh
+export EXTRAE_CONFIG_FILE=/usr/local/share/example/OMP/extrae.xml
+#export EXTRAE_CONFIG_FILE=/mnt/netapp1/Optcesga_FT2/opt/cesga/easybuild-cesga/software/MPI/gcc/6.4.0/openmpi/2.1.1/extrae/3.5.2/share/example/OMP/extrae.xml
+export LD_PRELOAD=/usr/local/lib/libomptrace.so
+#export LD_PRELOAD=/mnt/netapp1/Optcesga_FT2/opt/cesga/easybuild-cesga/software/MPI/gcc/6.4.0/openmpi/2.1.1/extrae/3.5.2/lib/libomptrace.so
 for var in "$@"
 do
     ./$var
